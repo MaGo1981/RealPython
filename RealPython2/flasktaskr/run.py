50294b8a4a72b79860b39e3aeddf97a68d2f9f01
Marko G <28060810+MaGo1981@users.noreply.github.com>
2018-03-24 05:11:27 +0100

    Docstring update

--- RealPython2/flasktaskr/run.py
+++ RealPython2/flasktaskr/run.py
@@ -1,4 +1,21 @@
 # project/run.py
 
+
+'''
+To start, this application has the following features:
+    
+1. Users can sign in and out from the landing page
+
+2. New users can register on a separate registration page
+
+3. Once signed in, users can add new tasks (each task consists of a name, 
+   due date, priority, status, and a unique ID)
+
+4. Users can view all incomplete tasks from the same page
+
+5. Users can also delete tasks and mark tasks as complete 
+(deleted tasks will be removed from the database)
+'''
+
 from views import app
 app.run(debug=True)
