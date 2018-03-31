ba66da49e092e5667a9a6cdb86b25fa6d937545d
Marko G <28060810+MaGo1981@users.noreply.github.com>
2018-03-31 09:22:48 +0200

    Update db_create.py for SQLAlchemy
     Since we are now using SQLAlchemy we're modifying the way we do database queries

--- RealPython2/flasktaskr/db_create.py
+++ RealPython2/flasktaskr/db_create.py
@@ -1,29 +1,21 @@
 # project/db_create.py
 
-import sqlite3
-from _config import DATABASE_PATH
+from views import db
+from models import Task
+from datetime import date
 
-with sqlite3.connect(DATABASE_PATH) as connection:
-    
-    # get a cursor object used to execute SQL commands
-    c = connection.cursor()
-    
-    # delete table if already exists (dodano mimo knjige)
-    c.execute('''
-    DROP TABLE IF EXISTS tasks
-    ''')
-    
-    # create the table
-    c.execute("""CREATE TABLE tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL, due_date TEXT NOT NULL, priority INTEGER NOT NULL,
-    status INTEGER NOT NULL)""")
-    
-    # insert dummy data into the table
-    c.execute(
-    'INSERT INTO tasks (name, due_date, priority, status)'
-    'VALUES("Finish this tutorial", "03/25/2015", 10, 1)'
-    )
-    c.execute(
-    'INSERT INTO tasks (name, due_date, priority, status)'
-    'VALUES("Finish Real Python Course 2", "03/25/2015", 10, 1)'
-    )
+
+# create the database and the db table
+# we initialize the database schema (i.e., Task) by calling db.create_all()
+db.create_all()
+
+
+# insert data
+# We then populate the table with some data, via the Task object from models.py to specify the schema.
+db.session.add(Task("Finish this tutorial", date(2016, 9, 22), 10, 1))
+db.session.add(Task("Finish Real Python", date(2016, 10, 3), 10, 1))
+
+
+# commit the changes
+# To apply the previous changes to our database we need to commit using db.session.commit()
+db.session.commit()
